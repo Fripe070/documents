82bf9cda5defbd69018995c60fa01716565b6689
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-06-09 20:20:43 +0200

    Auto-compilation action (#2)
    * Add action to compile typst documents into PDFs
    
    * more specific artifact name

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+find . -name "*.typ" ! -wholename "./templates/*" -print0 | while IFS= read -r -d '' file; do
+    rel="${file#./}"
+    output="./build/${rel%.typ}.pdf"
+    echo "Compiling $file to $output"
+    # Create all parent directories
+    mkdir -p "$(dirname "$output")"
+    # Compile
+    typst compile \
+        --root . \
+        --ignore-system-fonts \
+        --font-path assets/fonts/ \
+        --format pdf \
+        "$file" "$output"
+done
